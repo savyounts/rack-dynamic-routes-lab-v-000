b8d2852b5fff5e3ff8e36bf0585fe133fcbbb0b3
Sav Younts <savyounts@gmail.com>
2018-08-27 23:30:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      item = req.params["item"]
+      item = req.path.match(/items/).split("/items/")
       if Item.items.include?(item)
       resp.write "#{item.price}"
       else
