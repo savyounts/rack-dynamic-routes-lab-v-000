73065dc6445795b4fd1ac3a932a0dc4cb6dba98b
Sav Younts <savyounts@gmail.com>
2018-08-27 23:32:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      if Item.items.include?(item)
+      if @@items.include?(item)
       resp.write "#{item.price}"
       else
         resp.write "Route not found"
