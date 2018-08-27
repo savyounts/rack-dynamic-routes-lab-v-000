d051df26f5585680792c170b7be559651d8eb842
Sav Younts <savyounts@gmail.com>
2018-08-27 23:33:32 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,8 +6,8 @@ class Application
 
     if req.path.match(/items/)
       item = req.path.split("/items/").last
-      if @@items.include?(item)
-      resp.write item.price
+      if @@items.find{|i| i.name == item}
+        resp.write item.price
       else
         resp.write "Route not found"
         resp.status = 404
