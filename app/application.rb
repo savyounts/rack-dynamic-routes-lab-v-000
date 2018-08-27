0cef05b8e8101563f4fce706379dd052be778b81
Sav Younts <savyounts@gmail.com>
2018-08-27 23:23:32 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application 
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if path.match(/items/)
+      item = req.params["item"]
+      resp.write "#{item.price}"
+    else
+      resp.write "Item not found"
+      resp.status 400
+    end
+
+    resp.finish
+      
+end
